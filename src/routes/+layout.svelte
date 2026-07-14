a33e067c1b924dc98bd7cc965de06ebdced62208
Levi Zitting <me@levizitting.com>
2026-07-13 23:21:57 -0500

    feat: install generated favicon package

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,12 @@
 
 <svelte:head>
 	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+	<link rel="icon" href="/favicon-96x96.png" type="image/png" sizes="96x96" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+	<meta name="apple-mobile-web-app-title" content="ElectroNuck" />
+	<meta name="theme-color" content="#03050C" />
+	<link rel="manifest" href="/site.webmanifest" />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link
