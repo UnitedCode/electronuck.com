1add27c907966ea9c743f5c9733525224370a492
Levi Zitting <me@levizitting.com>
2026-07-13 23:18:18 -0500

    feat: add electronuck favicon artwork

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link
